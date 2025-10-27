d82f073c7f2c0f90840a84231165a0f07e8d94f0
kositzinsky <kositzinsky@gmail.com>
2025-10-27 16:37:10 +0500

    Complete task

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from gameparts import Board
-from gameparts.exeptions import FieldIndexError
+from gameparts.exeptions import FieldIndexError, CellOccupiedError
 
 
 def main():
@@ -18,24 +18,42 @@ def main():
                 column = int(input("Введите номер столбца: "))
                 if 0 < column >= game.field_size:
                     raise FieldIndexError
+
+                if game.board[row][column] != " ":
+                    raise CellOccupiedError
+
             except FieldIndexError:
                 print(
                     f"Значение должно быть неотрицательным и меньше {game.field_size}."
                 )
                 print("Пожалуйста, введите значения для строки и столбца заново.")
                 continue
+
             except ValueError:
                 print("Буквы вводить нельзя. Только числа.")
                 print("Пожалуйста, введите значения для строки и столбца заново.")
                 continue
+
             except Exception as e:
                 print(f"Возникла ошибка: {e}")
                 continue
+
             else:
                 break
+
         game.make_move(row, column, current_player)
         print("Ход сделан!")
         game.display()
+
+        if game.chek_win(current_player):
+            print(f"Победил игрок: {current_player}")
+            running = False
+
+        if game.is_board_full():
+            print("Ничья")
+            running = False
+
+        
         current_player = "O" if current_player == "X" else "X"
 
 
